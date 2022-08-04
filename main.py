2ddf0a99401c4b13b962d5232cfbb6d62f7cb050
Antonio Bernal Martinez <antoniobernalmartinez7@gmail.com>
2022-08-04 19:10:47 +0200

    working version for test

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import itertools
 
-class SnakeGame:
+class SnakeGame(object):
+
     def __init__(self, rows, columns, snake):
         self.rows = rows
         self.columns = columns
         self.snake = snake
+        self.board = None
         self.directions = {'U': [0, -1], 'D': [0, 1], 'L': [-1, 0], 'R': [1, 0]}
         self.add_snake()
+
 
     def add_snake(self):
         self.board = [[0 for i in range(self.columns)] for j in range(self.rows)]
         for i in range(len(self.snake)):
             self.board[self.snake[i][0]][self.snake[i][1]] = 1
 
-    def is_valid_move(self, i, j):
+    def is_valid_move(self, i, j, tail):
+        # check if the new head is out of bounds
         if (0 <= i < self.rows) and (0 <= j < self.columns):
-            if not self.board[i][j]:
+            # check if the new head is the tail and not part of the snake 
+            if (not self.board[i][j]) or ([i, j] == tail):
                 return True
         return False
 
@@ -23,10 +28,11 @@ class SnakeGame:
         movement = self.directions[direction]
         # get the head of the snake
         head = self.snake[0]
+        tail = self.snake[-1]
         # get the new head of the snake
         new_head = [head[0] + movement[0], head[1] + movement[1]]
         # check if the new head is valid
-        if self.is_valid_move(new_head[0], new_head[1]):
+        if self.is_valid_move(new_head[0], new_head[1], tail):
             # if valid, add the new head to the snake
             self.snake.insert(0, new_head)
             # remove the tail of the snake
@@ -45,16 +51,20 @@ class SnakeGame:
         print()
 
 
-board = SnakeGame(4, 3, [[2, 2], [3, 2], [3, 1], [3, 0], [2, 0], [1, 0], [0, 0]])
-board.print_board()
-print(board.move_snake('L'))
-board.print_board()
 
-# depth = 2
-# all_combinations = ["".join(item) for item in itertools.product("UPLR", repeat=depth)]
+all_combinations = ["".join(item) for item in itertools.product("UDLR", repeat=4)]
+total_paths = 0
 
-# for comb in all_combinations:
-#     for c in comb:
-#         print(c)
+for comb in all_combinations:
+    board = SnakeGame(10, 10, [[5,5], [5,4], [4,4], [4,5]])
+    good_path = True
 
+    for direction in comb:
+        if not board.move_snake(direction):
+            good_path = False
+            break
+    
+    if good_path:
+        total_paths += 1
 
+print(total_paths)
